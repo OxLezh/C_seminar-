c61ad65b4d8d14009916ae6dbfe3f36c8748b06d
Oxana Lezhneva <pythonpride@ya.ru>
2022-08-30 14:04:54 +0300

    seminar 3

---
+++ seminar_3/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Write("Введите координату x для точки A и нажмите клавишу Enter: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y для точки A и нажмите клавишу Enter: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z для точки A и нажмите клавишу Enter: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x для точки B и нажмите клавишу Enter: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y для точки B и нажмите клавишу Enter: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z для точки B и нажмите клавишу Enter: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+int degree = 2;
+int [] A = new int [] {x1, y1, z1};
+int [] B = new int [] {x2, y2, z2} ;
+double result = Math.Round(Math.Sqrt(Math.Pow((B[0] - A[0]), degree) + Math.Pow((B[1] - A[1]), degree) + Math.Pow((B[2] - A[2]), degree)), 3);
+Console.WriteLine("Расстояние между точками A и B: " + result);
---
+++ seminar_3/task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+Console.Write("Введите число и нажмите клавишу Enter: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+for (int i = 1; i<N+1; i++)
+{
+    array[i-1] = i * i * i;
+}
+var str = string.Join(" ", array);
+Console.WriteLine(str);
